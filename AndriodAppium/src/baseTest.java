4d5d69dc325f9cdf630dd98079a3598fc18b8213
abc <sudheerqa.selenium@gmail.com>
2020-07-30 11:45:25 -0500

    Cloned and commiting local project to GITHUB

---
+++ AndriodAppium/src/baseTest.java
@@ -0,0 +1,11 @@
+import java.net.MalformedURLException;
+
+public class baseTest {
+
+	public static void main(String[] args) throws MalformedURLException {
+	base cc= new base();
+	cc.capabilities();
+
+	}
+
+}
